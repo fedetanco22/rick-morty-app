9459f51f1b46585b477e2cb6a5d4cb1f9c5cf551
Federico Tanco”git config --global user.email fedetanco84@gmail.comgit config --global user.name Federico <fedetanco84@gmail.com>
2021-02-02 19:06:17 -0300

    EtilosContainers

--- src/components/CharactersCards.js
+++ src/components/CharactersCards.js
@@ -4,9 +4,9 @@ const CharactersCards = ({ character }) => (
   <div className="card-character">
     <img src={character.image} alt="character"></img>
     <div className="card-character-footer">
-      <h1>Nombre: {character.name}</h1>
-      <h2>Estatus: {character.status} </h2>
-      <h3>Especie: {character.species}</h3>
+      <p className="card-texts">Nombre: {character.name}</p>
+      <p className="card-texts">Estatus: {character.status} </p>
+      <p className="card-texts">Especie: {character.species}</p>
     </div>
   </div>
 );
--- src/components/EpisodesCards.js
+++ src/components/EpisodesCards.js
@@ -9,9 +9,9 @@ export default function EpisodesCards({ episodio }) {
           src="https://upload.wikimedia.org/wikipedia/en/thumb/b/b8/Rick_and_Morty_season_1.png/250px-Rick_and_Morty_season_1.png"
           alt="img season 1"></img>
         <div className="card-character-footer">
-          <p>Episodio: {episode}</p>
-          <p>Nombre del Espisodio: {name}</p>
-          <p>Dia de Estreno: {air_date}</p>
+          <p className="card-texts">Episodio: {episode}</p>
+          <p className="card-texts">Nombre del Espisodio: {name}</p>
+          <p className="card-texts">Dia de Estreno: {air_date}</p>
         </div>
       </div>
     </>
